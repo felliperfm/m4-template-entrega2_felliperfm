d5c1051436cc97ec4f22c425f821a65e695b2bdf
felliperfm <felliperfm@gmail.com>
2024-07-13 18:40:09 -0300

    Semicolons

--- src/middlewares/isBookIdValid.middleware.ts
+++ src/middlewares/isBookIdValid.middleware.ts
@@ -6,7 +6,7 @@ export class IsBookIdValid {
     static execute(req: Request, res: Response, next: NextFunction) {
         if (!booksDatabase.some(book => book.id === Number(req.params.id))) {
             throw new AppError(404, "Book not found.");
-        }
+        };
 
         next();
     };
--- src/middlewares/isBookNameValid.middleware.ts
+++ src/middlewares/isBookNameValid.middleware.ts
@@ -6,7 +6,7 @@ export class IsBookNameValid {
     static execute(req: Request, res: Response, next: NextFunction) {
         if (booksDatabase.some(book => book.name === req.body.name)) {
             throw new AppError(409, "Book already registered.");
-        }
+        };
 
         next();
     };
